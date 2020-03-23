7a49225aa3d68ce2246c8955485999bbd842a21b
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-03-23 23:31:09 +0000

    task 2 done

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+# create a manifest that kills a process named killmenow
+exec { "killmenow":
+    path => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+    command => "pkill -f killmenow",
+    provider => 'shell',
+}
