4ce9be71fb5117440826bfe56f53052ac0a0f1f0
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-03-24 17:26:03 +0000

    trying advanced task

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# using Puppet to make changes to our configuration file
+file_line {'add identity file':
+  path  => '/etc/ssh/ssh_config',
+  line   => ' IdentityFile ~/.ssh/holberton',
+  ensure => 'present',
+}
+file_line {'change pswrd aut':
+  path  => '/etc/ssh/ssh_config',
+  line   => ' PasswordAuthentication no',
+  match => 'PasswordAuthentication yes',
+  ensure => 'present',
+}
