4c29cbb8a1b6421b2fbdade3844520ca3e7af29d
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-03-25 08:04:12 +0000

    trying the advanced 6

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,4 +1,5 @@
-# configuring your server with Puppet
+# configuring your nginx server with Puppet
+
 package { 'nginx':
   ensure  => installed,
   name    => 'nginx',
@@ -9,13 +10,8 @@ file { '/var/www/html/index.html':
   content => 'Holberton School',
 }
 
-file { '/usr/share/nginx/html/index.html':
-  content => 'Holberton School',
-  path    => '/usr/share/nginx/html/index.html'
-}
-
 file { '/etc/nginx/sites-available/default':
-  path  => '/etc/nginx/sites-available/default',
+  path    => '/etc/nginx/sites-available/default',
   content => 'server {
     listen 80 default_server;
     listen [::]:80 default_server;
